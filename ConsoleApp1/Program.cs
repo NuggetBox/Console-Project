ad9d618a25fb851c6afc438761cc910bce03733b
Benjamin Ek <benjamin.ek1@learnet.se>
2018-05-21 17:07:26 +0200

    Setup

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int level;
+            float health, armor, damage, totalXp, xp;
+            bool inGame = true;
+            string gameName = "Heroville", pressKey = "Press any key to continue...";
+
+            Random random = new Random();
+           
+            while (inGame)
+            {
+                Console.WriteLine("Welcome to " + gameName + "!");
+                Console.WriteLine(pressKey);
+                Console.ReadKey(true);
+
+                //Console.ReadKey(true);
+            }
+
+            Console.ReadKey(true);
+        }
+
+        void Intro()
+        {
+            Console.WriteLine("");
+            Console.ReadKey(true);
+        }
+
+        void Loot()
+        {
+
+        }
+
+        void Upgrade()
+        {
+
+        }
+    }
+}
